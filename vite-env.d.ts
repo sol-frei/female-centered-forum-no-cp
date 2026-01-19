2e9e2b3e1b4a1b9c76d6d99479bdec14df48ae87
Frei <armerberkeyharry@outlook.com>
2026-01-19 10:53:04 +0000

    修改vite.env

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_ANON_KEY: string
   readonly VITE_ADMIN_PASSWORD: string
+  readonly VITE_SUPABASE_SERVICE_ROLE_KEY: string // 必须加上这一行
 }
 
 interface ImportMeta {
