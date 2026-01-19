1f0cbcd1d9104ae027ab6e40a18cda8d3806ed37
Frei <armerberkeyharry@outlook.com>
2026-01-19 12:42:49 +0000

    测试

--- api/admin/create-user.ts
+++ api/admin/create-user.ts
@@ -6,7 +6,7 @@ const supabaseUrl = process.env.VITE_SUPABASE_URL
 const supabaseServiceRoleKey = process.env.VITE_SUPABASE_SERVICE_ROLE_KEY
 
 if (!supabaseUrl || !supabaseServiceRoleKey) {
-  throw new Error('Supabase 环境变量缺失！请检查 Vercel 设置')
+  throw new Error('Supabase 环境变量缺失！请检查 Vercel 设置。')
 }
 
 const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey)
