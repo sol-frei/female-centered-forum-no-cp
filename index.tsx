ae20386d97823207cf209fe0163b3da83ab97324
Frei <armerberkeyharry@outlook.com>
2026-01-20 05:57:01 +0000

    修改收藏组件跳转问题

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { BrowserRouter } from 'react-router-dom';
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
@@ -11,5 +11,17 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* Router 包裹整个 App */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
