92916e0b55db0419f401671d1690fb41667c4d08
Frei <armerberkeyharry@outlook.com>
2026-01-20 10:15:36 +0000

    测试

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
