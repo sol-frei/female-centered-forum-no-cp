6632a4737f15028b6d6e10776de7e3099603d21a
Frei <armerberkeyharry@outlook.com>
2026-01-22 14:36:46 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -395,8 +395,7 @@ const PostDetail = ({
   return (
     <div className="flex flex-col min-h-screen">
       <div className="w-full py-8 px-0 flex-1 pb-32 relative">
-        <button onClick={onBack} className="sticky top-[7.5rem] md:top-14 z-40 w-full text-left py-2 px-4 bg-white/80 backdrop-blur-md mb-4 text-sm text-zinc-500 hover:text-black transition-all border-b border-transparent hover:border-zinc-100">← 返回列表</button>
-
+        <button onClick={onBack} className="sticky top-[7.5rem] md:top-14 z-40 w-full text-left py-2 px-4 bg-white/80 backdrop-blur-md text-sm text-zinc-500 hover:text-black transition-all border-b border-transparent hover:border-zinc-100">← 返回列表</button>
         {/* 帖子内容 */}
          <div className="bg-white border-t border-b border-zinc-200 shadow-sm mb-6">
           <div className="px-3 py-4">
