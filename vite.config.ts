beb43ec9636b29f2b81f844c37b37fa9d37ac91b
Frei <armerberkeyharry@outlook.com>
2026-02-22 01:38:07 +0000

    修改语法错误

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
